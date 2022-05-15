f12f98dd6663ac3b00d0746b4db4de21c85e1c51
ryuken73 <ryuken73@gmail.com>
2022-05-15 23:33:09 +0900

    update: add tast start button

--- src/renderer/Components/Pages/MainTab/TabButtons.jsx
+++ src/renderer/Components/Pages/MainTab/TabButtons.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 import Box from '@mui/material/Box';
+import PlayCircleIcon from '@mui/icons-material/PlayCircle';
 import CheckIcon from '@mui/icons-material/Check';
 import DeleteIcon from '@mui/icons-material/Delete';
 import ButtonIcon from 'renderer/Components/Common/ButtonIcon';
 import useJobListState from 'renderer/hooks/useJobListState';
 
+const Container = styled.div`
+  display: flex;
+`
 const ButtonContainer = styled(Box)`
   display: flex;
   flex-direction: row;
-  justify-content: flex-end;
+  justify-content: flex-start;
   height: ${(prop) => prop.height || 'auto'};
   width: ${(prop) => prop.height || 'auto'};
   .MuiSvgIcon-root {
@@ -21,13 +25,23 @@ const ButtonContainer = styled(Box)`
 const TabButtons = () => {
   const { removeJobAllCheckedState } = useJobListState();
   return (
-    <ButtonIcon
-      text="삭제"
-      iconComponent={<DeleteIcon />}
-      border="1px solid rgba(255, 255, 255, .5)"
-      hoverBorder="1px solid rgba(255, 255, 255, 0.8)"
-      onClick={removeJobAllCheckedState}
-    />
+    <ButtonContainer>
+      <ButtonIcon
+        text="작업시작"
+        iconComponent={<PlayCircleIcon />}
+        border="1px solid rgba(255, 255, 255, .5)"
+        hoverBorder="1px solid rgba(255, 255, 255, 0.8)"
+        onClick={removeJobAllCheckedState}
+      />
+      <ButtonIcon
+        text="삭제"
+        iconComponent={<DeleteIcon />}
+        border="1px solid rgba(255, 255, 255, .5)"
+        hoverBorder="1px solid rgba(255, 255, 255, 0.8)"
+        onClick={removeJobAllCheckedState}
+      />
+    </ButtonContainer>
+
   );
 };
 
