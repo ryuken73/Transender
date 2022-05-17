6f5a480e5ad8ae94dbade06abf918e67dd74f239
ryuken73 <ryuken73@gmail.com>
2022-05-18 07:46:02 +0900

    fix: apply ellipse to footer app message while keeping occuping all remain width

--- src/renderer/Components/Footer/index.jsx
+++ src/renderer/Components/Footer/index.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
 import useAppState from 'renderer/hooks/useAppState';
+import TextBox from 'renderer/Components/Common/TextBox';
 import { getVersion } from 'renderer/lib/appUtil';
 
 const Container = styled.div`
   width: 100%;
-  display: flex;
   padding: 5px;
+  display: flex;
+  align-items: center;
 `;
 const AppMessage = styled.div`
-  margin-right: auto;
+  /* margin-right: auto; */
+  display: flex;
+  align-items: center;
+  min-width: 0;
+  flex-shrink: 1;
+  margin-left: 5px;
+`
+const CustomTextBox = styled(TextBox)`
+  font-size: 12px;
+  opacity: 0.8;
+  margin-right: 5px;
+`
+const CustomTextBoxLimitWidth = styled(CustomTextBox)`
+  /* max-width: 90%; */
+  min-width: 0;
 `
 const Version = styled.div`
   margin-left: auto;
@@ -28,9 +44,16 @@ const Footer = () => {
     <Container>
       {/* <AppMessage>[{message.level}][{message.text}]</AppMessage> */}
       <AppMessage>
-        [{appLog.level}] [{appLog.date}] {appLog.message}
+        <CustomTextBox text={`[${appLog.level}]`} />
+        <CustomTextBox text={`[${appLog.date}]`} />
+        <CustomTextBoxLimitWidth
+          containerProps={{ minWidth: 0 }}
+          text={appLog.message}
+        />
       </AppMessage>
-      <Version>v{version}</Version>
+      <Version>
+        <CustomTextBox text={`v${version}`} />
+      </Version>
     </Container>
   )
 };
