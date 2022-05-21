ec4ee964d078b23248ca855a2962c319acd9bb0f
ryuken73 <ryuken73@gmail.com>
2022-05-21 22:22:49 +0900

    update: call app.getAppInfo() by IpcRenderer.invoke and response (use appUitl.js)

--- src/main/appUtil.js
+++ src/main/appUtil.js
@@ -1,9 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 const { ipcMain, app } = require('electron');
+const pathKinds = ['home', 'appData', 'userData', 'temp', 'downloads', 'logs'];
 
 const setupIPCHandlers = () => {
   ipcMain.handle('getVersion', () => {
     return Promise.resolve(app.getVersion());
   });
-}
+  ipcMain.handle('getAppInfo', (event) => {
+    const paths = pathKinds.reduce((acct, path) => {
+      return {
+        ...acct,
+        [path]: app.getPath(path),
+      };
+    }, {});
+    const result = {
+      paths,
+      appName: app.name,
+    }
+    return Promise.resolve(result);
+  });
+};
 
 module.exports = setupIPCHandlers;
--- src/renderer/lib/appUtil.js
+++ src/renderer/lib/appUtil.js
@@ -3,7 +3,11 @@ const { ipcRenderer } = require('electron');
 const getVersion = async () => {
   return ipcRenderer.invoke('getVersion');
 };
+const getPath = async (path='home') => {
+  return ipcRenderer.invoke('getPath', path);
+};
 
 module.exports = {
   getVersion,
+  getPath,
 };
